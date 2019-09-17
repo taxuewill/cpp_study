0ee5a6dc3c02f4471cc99e54ac037ad3ec587188
jing wang2 <jing.wang2@ninebot.com>
2019-09-17 19:05:17 +0800

    [cpp]test ReaderWriterQueue

--- src/test/test1.cpp
+++ src/test/test1.cpp
@@ -1,11 +1,18 @@
 #include <iostream>
 
+#include "moodycamel/readerwriterqueue.h"
+
+using namespace moodycamel;
 
 using namespace std;
 
 int main(int argc,char** argv){
     cout<<"Hello,cpp"<<endl;
-    
+    ReaderWriterQueue<int> q(100);
+    q.enqueue(17);
+    int number;
+    bool flag = q.try_dequeue(number);
+    cout<<"flag "<<flag<<",number is "<<number<<endl;
 
     return 0;
 }
