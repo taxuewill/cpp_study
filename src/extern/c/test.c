15181d00a771f18ce7266692091e2c4c0dc6a6cc
jing wang2 <jing.wang2@ninebot.com>
2020-01-17 09:26:27 +0800

    append

---
+++ src/extern/c/test.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+
+int main(){
+    /*this is comment*/
+    printf("hello,C\n");
+    return 0;
+}
---
+++ src/extern/cpp_link_c/add.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int add(const int a, const int b)
+{
+    return (a + b);
+}
