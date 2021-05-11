1a3711f4f4372fcebee12817479f008a77efab1c
jing wang2 <jing.wang2@ninebot.com>
2021-05-11 15:56:13 +0800

    [cpp]optimize

--- src/test/test1.cpp
+++ src/test/test1.cpp
@@ -1,25 +1,27 @@
-  #include <vector>
-  #include <iostream>
-  #include <thread>
-  #include <atomic>
-  #include <string>
- 
-  std::atomic<int> cnt = {0};
- using namespace std;
-  void f()
+#include <vector>
+#include <iostream>
+#include <thread>
+#include <atomic>
+#include <string>
+
+std::atomic<int> cnt = {0};
+using namespace std;
+void f()
+{
+  for (int n = 0; n < 1000; ++n)
   {
-    for (int n = 0; n < 1000; ++n) {
-        cnt.fetch_add(1, std::memory_order_relaxed);
-    }
+    cnt.fetch_add(1, std::memory_order_relaxed);
   }
- 
-  int main()
-  {
+}
+
+int main()
+{
   //  const char* s1 = "hello";
-   string s1("hello");
-   string s2("hello");
-   if(s1==s2){
-     cout<<"equals"<<endl;
-   }
-    cout<<s1<<endl;
+  string s1("hello");
+  string s2("hello");
+  if (s1 == s2)
+  {
+    cout << "equals" << endl;
   }
+  cout << s1 << endl;
+}
